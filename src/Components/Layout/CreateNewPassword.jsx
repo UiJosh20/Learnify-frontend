7b36e45a99b9dc7a7f4de28de5b113439a03b2a0
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-02-29 11:50:15 +0100

    done with the forgotten password interface

--- src/Components/Layout/CreateNewPassword.jsx
+++ src/Components/Layout/CreateNewPassword.jsx
@@ -11,6 +11,7 @@ const CreateNewPassword = () => {
 
     const { handleChange, handleSubmit, values, errors } = useFormik({
         initialValues: {
+            email: "",
             password: "",
         },
         validationSchema: createNewPassword,
@@ -39,7 +40,7 @@ const CreateNewPassword = () => {
         <>
         <section className="flex justify-center items-center lg:p-48 bg-slate-900 p-5 h-screen">
             <form onSubmit={handleSubmit} className=" w-96">
-                {/* <h1 className="mb-5">Forget password</h1> */}
+                <input type="email" placeholder='Enter email address' onChange={handleChange} name="email" value={values.email} className="w-full mb-3 p-3 bg-slate-100 rounded-md" /> <span className="text-red-500">{errors.email}</span>
                 <input type="text" placeholder='Enter a new password' onChange={handleChange} name="password" value={values.password} className="w-full mb-3 p-3 bg-slate-100 rounded-md" /> <span className="text-red-500">{errors.password}</span>
                 <button type="submit" className="bg-blue-500 p-3 text-white rounded w-full mb-3 font-bold" disabled={buttonText === "Changing old password..."}>{buttonText}</button>
             </form>
--- src/Components/Schema/createNewPasswordSchema.js
+++ src/Components/Schema/createNewPasswordSchema.js
@@ -1,6 +1,7 @@
 import * as yup from 'yup';
 
 export const createNewPassword = yup.object({
+    email: yup.string().email('Invalid email address').required('Email is required'),
     password: yup.string().required('Password is required').min(8, 'Password must be at least 8 characters').matches(
         /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$/,
         "Password must be at least 8 characters long and contain both letters and numbers"
